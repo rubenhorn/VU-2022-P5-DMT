43e2280cfa74feb1fd87ed3a4344b55330aae6a3
Ruben Horn <horn.ruben@yahoo.de>
2022-04-16 20:44:27 +0200

    print corr

--- assignment_1_basic/task_1_loan/2_eda.py
+++ assignment_1_basic/task_1_loan/2_eda.py
@@ -8,16 +8,25 @@ input_dataset_filename = (Path(__file__).parent / 'german-pre-processed.csv').re
 df = pd.read_csv(input_dataset_filename)
 
 print('Number of samples:', len(df))
+print('Number of features:', len(df.columns) - 1)
+print('Highest targte correlation coefficients:')
+Xy_correlation = df.corrwith(df['good_customer'])
+Xy_correlation = Xy_correlation.sort_values(ascending=False, key=abs)
+# Exclude auto-correlation (1.0)
+Xy_correlation = Xy_correlation.drop(Xy_correlation.index[0])
+print(Xy_correlation.head().to_string())
+
+print('Done'); exit()
 
 # Only look at a few columns at a time or the plot will be too big
-columns = ['age', 'credits_count', 'installment_rate_relative_to_income', 'providing_for_count', 'registered_telephone', 'good_customer']
+columns = ['age', 'credits_count', 'installment_rate_relative_to_income', 'registered_telephone', 'good_customer']
 
-# plot the distribution for each column
-for column in columns:
-    plt.figure()
-    sns.distplot(df[column])
-    plt.title(f'{column}')
-    plt.show()
+fig, axes = plt.subplots(nrows=1, ncols=len(columns), figsize=(20, 5))
+for i, column in enumerate(columns):
+    sns.distplot(df[column], ax=axes[i])
+    axes[i].set_title(column)
+plt.tight_layout()
+plt.show()
 
 
 sns.heatmap(df[columns].corr(), annot=True, cmap='coolwarm')
