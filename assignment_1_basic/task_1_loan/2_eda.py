02ad0f24e8050d45737fad5469bba536582dfa7c
Ruben Horn <horn.ruben@yahoo.de>
2022-04-16 20:20:44 +0200

    cross validation

--- assignment_1_basic/task_1_loan/2_eda.py
+++ assignment_1_basic/task_1_loan/2_eda.py
@@ -7,6 +7,8 @@ input_dataset_filename = (Path(__file__).parent / 'german-pre-processed.csv').re
 
 df = pd.read_csv(input_dataset_filename)
 
+print('Number of samples:', len(df))
+
 # Only look at a few columns at a time or the plot will be too big
 columns = ['age', 'credits_count', 'installment_rate_relative_to_income', 'providing_for_count', 'registered_telephone', 'good_customer']
 
---
+++ assignment_1_basic/task_1_loan/3_cross_validation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.utils import shuffle
+
+input_dataset_filename = (Path(__file__).parent / 'german-pre-processed.csv').resolve()
+
+random_state = 42
+kfold_splits = 10
+n_neighbors = 5
+n_trees = 10
+
+model_builders = {
+    'LinearSVC': lambda: LinearSVC(random_state=random_state, dual=False),
+    'KNeighborsClassifier': lambda: KNeighborsClassifier(n_neighbors=n_neighbors),
+    'RandomForestClassifier': lambda: RandomForestClassifier(random_state=random_state, n_estimators=n_trees),
+}
+
+df = pd.read_csv(input_dataset_filename)
+X = df.drop('good_customer', axis=1)
+y = df['good_customer']
+X, y  = shuffle(X, y, random_state=random_state)
+
+def cost(y_true, y_pred):
+    costs = [5 if actual == 1 else 1 for expected, actual in zip(y_true, y_pred) if expected != actual]
+    return np.array(costs).sum() / len(y_true)
+scorer = make_scorer(cost, greater_is_better=False)
+
+def fit_and_score_model(model, train, test):
+    X_train, X_test, y_train, y_test = X.iloc[train], X.iloc[test], y.iloc[train], y.iloc[test]
+    model.fit(X_train, y_train)
+    return scorer(model, X_test, y_test)
+
+kf = KFold(n_splits=kfold_splits, random_state=random_state, shuffle=True)
+print('Model & Cost mean & Cost std \\\\')
+for model_name, model_builder in model_builders.items():
+    scores = np.array([fit_and_score_model(model_builder(), train, test) for train, test in kf.split(X, y)])
+    print(model_name, '&', scores.mean(), '&', scores.std(), '\\\\')
+
+print('Done')
