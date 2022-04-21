e81548e4c7cec90c53e73a3433c92d6cc65ba2c6
Abdellah Lahnaoui <abdellah.lahnaoui@gmail.com>
2022-04-21 04:22:28 +0200

    Add test dataset prep

--- assignment_1_basic/task_2_titanic/1_preparation.py
+++ assignment_1_basic/task_2_titanic/1_preparation.py
@@ -2,11 +2,17 @@ import pandas as pd
 import scipy.stats as st
 from pathlib import Path
 
-input_dataset_filename = (Path(__file__).parent / 'train.csv').resolve()
-output_dataset_filename = (Path(__file__).parent / 'train-pre-processed.csv').resolve()
+input_train_dataset_filename = (Path(__file__).parent / 'train.csv').resolve()
+output_train_dataset_filename = (Path(__file__).parent / 'train-pre-processed.csv').resolve()
+input_test_dataset_filename = (Path(__file__).parent / 'test.csv').resolve()
+output_test_dataset_filename = (Path(__file__).parent / 'test-pre-processed.csv').resolve()
 
-df = pd.read_csv(input_dataset_filename, delimiter=',')
-df = df.drop_duplicates()
+train_df = pd.read_csv(input_train_dataset_filename, delimiter=',')
+train_df = train_df.drop_duplicates()
+
+test_df = pd.read_csv(input_test_dataset_filename, delimiter=',')
+test_df = test_df.drop_duplicates()
+
 # Source: https://stackoverflow.com/questions/37487830/how-to-find-probability-distribution-and-parameters-for-real-data-python-3
 def get_best_distribution(data):
     dist_names = ["norm", "exponweib", "weibull_max", "weibull_min", "pareto", "genextreme"]
@@ -14,11 +20,11 @@ def get_best_distribution(data):
     params = {}
     for dist_name in dist_names:
         dist = getattr(st, dist_name)
-        param = dist.fit(df[data])
+        param = dist.fit(train_df[data])
 
         params[dist_name] = param
         # Applying the Kolmogorov-Smirnov test
-        D, p = st.kstest(df[data], dist_name, args=param)
+        D, p = st.kstest(train_df[data], dist_name, args=param)
         # print("p value for "+dist_name+" = "+str(p))
         dist_results.append((dist_name, p))
 
@@ -32,22 +38,25 @@ def get_best_distribution(data):
     return best_dist, best_p, params[best_dist]
 
 # Find empty values
-empty_vals = df.isnull().sum()
+empty_vals = train_df.isnull().sum()
 
 # Replace empty values
-df['Age'].fillna(df['Age'].mean(), inplace=True)
-df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+train_df['Age'].fillna(train_df['Age'].mean(), inplace=True)
+train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace=True)
+test_df['Age'].fillna(test_df['Age'].mean(), inplace=True)
+test_df['Embarked'].fillna(test_df['Embarked'].mode()[0], inplace=True)
+
 
 # Find correlation between columns
-col_corr = df.corr()
+col_corr = train_df.corr()
 
 # ===========================Distibution=============================   ==
-# print(df[['Age','SibSp','Parch','Fare']].describe())
-# print(df.describe(include=['O']))
+# print(train_df[['Age','SibSp','Parch','Fare']].describe())
+# print(train_df.describe(include=['O']))
 # col_names = ['Survived', 'Pclass', 'Sex', 'Embarked']
 # for col in col_names:
-#     # print(df.groupby(col).count())
-#     print(df.groupby(["Survived", col]).size())
+#     # print(train_df.groupby(col).count())
+#     print(train_df.groupby(["Survived", col]).size())
 
 
 # col_names = ['Age', 'Fare', 'Parch', 'SibSp'] # Add Age column
@@ -55,37 +64,53 @@ col_corr = df.corr()
 #     get_best_distribution(col)
 
 # Drop unecessary columns
-df = df.drop(['PassengerId','Name','Ticket', 'Cabin'], axis=1)
+train_df = train_df.drop(['PassengerId','Name','Ticket', 'Cabin'], axis=1)
+test_df = test_df.drop(['PassengerId','Name','Ticket', 'Cabin'], axis=1)
 
 # Convert Sex column to binary
-df['Sex'] = df['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+train_df['Sex'] = train_df['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+test_df['Sex'] = test_df['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
 
 # Convert Embarked column to ordinal values
-df['Embarked'] = df['Embarked'].map( {'C': 0, 'Q': 1, 'S': 2} ).astype(int)
+train_df['Embarked'] = train_df['Embarked'].map( {'C': 0, 'Q': 1, 'S': 2} ).astype(int)
+test_df['Embarked'] = test_df['Embarked'].map( {'C': 0, 'Q': 1, 'S': 2} ).astype(int)
 
 
 # Cut the age column into 5 categories
-# df['Age'] = pd.cut(df['Age'], 5)
-# grouped__age_df = df[['Age', 'Survived']].groupby(['Age'], as_index=False).mean().sort_values(by='Age', ascending=True)
+# train_df['Age'] = pd.cut(train_df['Age'], 5)
+# grouped__age_df = train_df[['Age', 'Survived']].groupby(['Age'], as_index=False).mean().sort_values(by='Age', ascending=True)
 # print(grouped__age_df)
 
 # Convert Age column to oridnal values
-df.loc[ df['Age'] < 17, 'Age'] = 0
-df.loc[(df['Age'] >= 17) & (df['Age'] < 33), 'Age'] = 1
-df.loc[(df['Age'] >= 33) & (df['Age'] < 49), 'Age'] = 2
-df.loc[(df['Age'] >= 49) & (df['Age'] < 65), 'Age'] = 3
-df.loc[ df['Age'] >= 65, 'Age'] = 4
+train_df.loc[ train_df['Age'] < 17, 'Age'] = 0
+train_df.loc[(train_df['Age'] >= 17) & (train_df['Age'] < 33), 'Age'] = 1
+train_df.loc[(train_df['Age'] >= 33) & (train_df['Age'] < 49), 'Age'] = 2
+train_df.loc[(train_df['Age'] >= 49) & (train_df['Age'] < 65), 'Age'] = 3
+train_df.loc[ train_df['Age'] >= 65, 'Age'] = 4
+
+test_df.loc[ test_df['Age'] < 17, 'Age'] = 0
+test_df.loc[(test_df['Age'] >= 17) & (test_df['Age'] < 33), 'Age'] = 1
+test_df.loc[(test_df['Age'] >= 33) & (test_df['Age'] < 49), 'Age'] = 2
+test_df.loc[(test_df['Age'] >= 49) & (test_df['Age'] < 65), 'Age'] = 3
+test_df.loc[ test_df['Age'] >= 65, 'Age'] = 4
+
 
 # Cut the Fare column into 3 categories
-# df['Fare'] = pd.cut(df['Fare'], 3)
-# grouped_fare_df = df[['Fare', 'Survived']].groupby(['Fare'], as_index=False).mean().sort_values(by='Fare', ascending=True)
+# train_df['Fare'] = pd.cut(train_df['Fare'], 3)
+# grouped_fare_df = train_df[['Fare', 'Survived']].groupby(['Fare'], as_index=False).mean().sort_values(by='Fare', ascending=True)
 # print(grouped_fare_df)
 
 # Convert Fare column to oridnal values
-df.loc[ df['Fare'] < 171, 'Fare'] = 0
-df.loc[(df['Fare'] >= 171) & (df['Fare'] < 342), 'Fare'] = 1
-df.loc[ df['Fare'] >= 342, 'Fare'] = 2
+train_df.loc[ train_df['Fare'] < 171, 'Fare'] = 0
+train_df.loc[(train_df['Fare'] >= 171) & (train_df['Fare'] < 342), 'Fare'] = 1
+train_df.loc[ train_df['Fare'] >= 342, 'Fare'] = 2
 
-df.to_csv(output_dataset_filename, index=False)
+test_df.loc[ test_df['Fare'] < 171, 'Fare'] = 0
+test_df.loc[(test_df['Fare'] >= 171) & (test_df['Fare'] < 342), 'Fare'] = 1
+test_df.loc[ test_df['Fare'] >= 342, 'Fare'] = 2
+
+
+train_df.to_csv(output_train_dataset_filename, index=False)
+test_df.to_csv(output_test_dataset_filename, index=False)
 
 print('Done')
