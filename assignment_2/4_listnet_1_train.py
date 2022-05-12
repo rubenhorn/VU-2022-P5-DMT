014a4b101cd5877c5daa8d05ac46b7027a32d6ab
Ruben Horn <5367484+rubenhorn@users.noreply.github.com>
2022-05-12 09:46:01 +0200

    Alternative algorithm: ListNet (#5)
    * added sub-module
    
    * tf implementation
    
    * verbose training
    
    * predict using listnet
    
    * fixed ListNet submission generation
    
    * minor changes
    
    * removed unused submodule
    
    * bugfix padding

---
+++ assignment_2/4_listnet_1_train.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+
+import tensorflow as tf
+
+import sys
+from utils import *
+from pathlib import Path
+from preprocessing import Preprocessing
+import hyperparameters as hp
+
+reset_timer()
+
+dataset_name = 'training_set_VU_DM'
+model_out_path = (Path(__file__).parent / 'models' / f'{dataset_name}-ListNet').resolve()
+
+train_set_name = dataset_name + '-train'
+test_set_name = dataset_name + '-test'
+if hp.use_small_dataset:
+    train_set_name += '-small'
+    test_set_name += '-small'
+    print('-' * 80)
+    print('WARNING: Using small dataset', file=sys.stderr)
+    print('Do not use for submission!', file=sys.stderr)
+    print('-' * 80)
+
+def create_batches(data):
+    X_batches = []
+    y_batches = []
+    for _, group in data.groupby('srch_id'):
+        group = group.sample(DOCS_PER_QUERY, replace=True) # Pad input
+        assert len(group) == DOCS_PER_QUERY
+        X = pp.transform(group)
+        y = group.apply(lambda row: row['booking_bool'] * 5 + row['click_bool'] * 1, axis=1)
+        X_batches.append(X)
+        y_batches.append(y)
+    return X_batches, y_batches
+
+pp = Preprocessing()
+train_set = load_dataset(train_set_name)
+test_set = load_dataset(test_set_name)
+
+tprint('Create batches by query...')
+X_train_batches, y_train_batches = create_batches(train_set)
+X_test_batches, y_test_batches = create_batches(test_set)
+
+def create_model(docs_per_query, embedding_dims):
+    # TODO refine network architecture
+    docs_input = tf.keras.layers.Input(shape=(docs_per_query, embedding_dims, ), dtype=tf.float32, name='docs')
+    dense_1 = tf.keras.layers.Dense(units=3, activation='linear', name='dense_1')
+    dense_out = tf.keras.layers.Dense(units=1, activation='linear', name='scores')
+    scores_prob_dist = tf.keras.layers.Dense(units=docs_per_query, activation='softmax', name='scores_prob_dist')
+    dense_1_out = dense_1(docs_input)
+    scores = tf.keras.layers.Flatten()(dense_out(dense_1_out))
+    model_out = scores_prob_dist(scores)
+    model = tf.keras.models.Model(inputs=[docs_input], outputs=[model_out])
+    optimizer = tf.keras.optimizers.SGD(learning_rate=hp.learning_rate, momentum=hp.momentum)
+    loss = tf.keras.losses.KLDivergence()
+    model.compile(optimizer=optimizer, loss=loss)
+    return model
+
+
+tprint('Creating ListNet...')
+embedding_dims = X_train_batches[0].shape[1]
+model = create_model(DOCS_PER_QUERY, embedding_dims)
+model.summary()
+
+tprint('Fitting model...')
+X_train_batches_array = np.array(X_train_batches)
+y_train_batches_tensor = tf.constant(y_train_batches, dtype=tf.float32)
+relevance_grades_prob_dist = tf.nn.softmax(y_train_batches_tensor, axis=-1)
+hist = model.fit(
+    [X_train_batches_array], 
+    relevance_grades_prob_dist, 
+    epochs=hp.tf_epochs, 
+    verbose=True,
+)
+
+tprint('Evaluating model...')
+X_test_batches_array = np.array(X_test_batches)
+y_test_batches_tensor = tf.constant(y_test_batches, dtype=tf.float32)
+relevance_grades_prob_dist = tf.nn.softmax(y_test_batches_tensor, axis=-1)
+loss = model.evaluate(
+    [X_test_batches_array],
+    relevance_grades_prob_dist,
+    verbose=False
+)
+print('Test loss:', loss)
+
+tprint('Freezing trained model...')
+model_out_path.mkdir(exist_ok=True, parents=True)
+model.save(str(model_out_path.absolute()))
+
+tprint('Done')
---
+++ assignment_2/4_listnet_2_generate_submission.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+from utils import *
+from pathlib import Path
+import tensorflow as tf
+
+from preprocessing import Preprocessing
+
+reset_timer()
+
+dataset_name = 'test_set_VU_DM'
+model_in_path = (Path(__file__).parent / 'models' /
+                 'training_set_VU_DM-ListNet').resolve()
+prediction_out_path = (Path(__file__).parent / 'output' /
+                       f'{dataset_name}-prediction.csv').resolve()
+
+test_set = load_dataset(dataset_name)
+tprint(f'Loading model from {model_in_path}...')
+model = tf.keras.models.load_model(model_in_path)
+start_time = time.time()
+tprint('Creating batches by grouping by srch_id...')
+# With padding
+pad_group = lambda group: pd.concat([group, group.sample(DOCS_PER_QUERY - len(group), replace=True)])
+grouped_test_set = [pad_group(group) for _, group in test_set.groupby('srch_id')]
+n_groups = len(grouped_test_set)
+tprint('Transforming input...')
+pp = Preprocessing()
+X_batches = []
+y_batches = []
+start_time = time.time()
+for i in range(len(grouped_test_set)):
+    group = grouped_test_set[i]
+    remaining_time = (time.time() - start_time) * (n_groups - i) / (i + 1)
+    tprint(f'Group {i + 1}/{n_groups} (Remaining: { format_time(remaining_time) })...', end='\r')
+    X = pp.transform(group)
+    X_batches.append(X)
+    # Alternative: Run inference on a per query basis (not recommended)
+    # X_minibatch = tf.expand_dims(tf.convert_to_tensor(X), axis=0)
+    # y_batch = model.predict(X_minibatch, batch_size=1)
+    # y_batches.append(y_batch)
+print()
+tprint('Generating prediction...')
+BATCH_SIZE = 32
+start_time = time.time()
+for i in range(0, len(X_batches), BATCH_SIZE):
+    remaining_time = (time.time() - start_time) * (len(X_batches) - i) / (i + 1)
+    tprint(f'Batch {i + 1}/{len(X_batches)} (Remaining: { format_time(remaining_time) })...', end='\r')
+    X_batch = X_batches[i:i + BATCH_SIZE] if i + BATCH_SIZE < len(X_batches) else X_batches[i:]
+    X_batch_tensor = tf.convert_to_tensor(np.array(X_batch))
+    y_batch = model.predict(X_batch_tensor, batch_size=BATCH_SIZE, use_multiprocessing=True)
+    y_batches.extend(y_batch)
+print()
+
+tprint('Sort properties by predicted score...')
+result = np.empty(shape=(2,), dtype=int)
+columns = ['srch_id', 'prop_id']
+start_time = time.time()
+for i in range(len(grouped_test_set)):
+    group = grouped_test_set[i]
+    scores = y_batches[i]
+    remaining_time = (time.time() - start_time) * (n_groups - i) / (i + 1)
+    tprint(f'Sorting group {i + 1}/{n_groups} (Remaining: { format_time(remaining_time) })...', end='\r')
+    group['score'] = scores
+    group.sort_values(by='score', ascending=False, inplace=True)
+    group.drop_duplicates(subset=['prop_id'], inplace=True) # Remove padding
+    result = np.vstack((result, group[columns].values))
+print()
+df = pd.DataFrame(result[1:], columns=columns)
+
+tprint(f'Saving prediction to {prediction_out_path}...')
+prediction_out_path.parent.mkdir(exist_ok=True)
+assert len(df) == len(test_set), f'Prediction size: {len(df)} != test set size: {len(test_set)}'
+df.to_csv(prediction_out_path, index=False)
+
+tprint('Done')
