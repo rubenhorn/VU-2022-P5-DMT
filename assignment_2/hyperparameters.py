0436758c839c5cc7a81e31932561663e2af2ce14
Abdellah Lahnaoui <abdellah.lahnaoui@gmail.com>
2022-05-25 13:09:59 +0200

    Increased num of estimators

--- assignment_2/hyperparameters.py
+++ assignment_2/hyperparameters.py
@@ -15,7 +15,8 @@ reduce_lr_patience=2
 min_lr=1e-6
 
 # LambdaMART
-lm_n_estimators = 100
+# lm_n_estimators = 100
+lm_n_estimators = 200
 lm_learning_rate = 0.2
 lm_max_leaf_nodes = 10
 lm_min_samples_leaf = 64
